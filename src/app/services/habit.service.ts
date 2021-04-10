c1460127aace8f42e6925821e98d53226c3b9ec6
dwyfrequency <jdwy215@gmail.com>
2021-04-10 10:34:14 -0400

    use subject to broadcast new values

--- src/app/services/habit.service.ts
+++ src/app/services/habit.service.ts
@@ -1,28 +1,28 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, BehaviorSubject } from 'rxjs';
 import { Habit } from '../shared/habit';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HabitService {
-  habits: Habit[] = [
+  subject = new BehaviorSubject<Habit[]>([
     { id: 1, name: 'Brush Teeth' },
     { id: 2, name: 'Eat Breakfast' },
     { id: 3, name: 'Do flashcards' },
-  ];
+  ]);
 
   constructor() {}
 
   getHabits(): Observable<Habit[]> {
-    return of(this.habits);
+    return this.subject.asObservable();
   }
 
-  removeHabit(id: number) {
-    this.habits = this.habits.filter((x) => x.id !== id);
+  removeHabit(id: number): void {
+    this.subject.next(this.subject.getValue().filter((x) => x.id !== id));
   }
 
-  addHabit(habit: Habit) {
-    this.habits.push(habit);
+  addHabit(habit: Habit): void {
+    this.subject.next(this.subject.getValue().concat(habit));
   }
 }

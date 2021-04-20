c5d85557f098ae4a82d15edcc99628153a112bde
dwyfrequency <jdwy215@gmail.com>
2021-04-19 21:32:29 -0400

    add some practice with element refs

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { of } from 'rxjs';
 
 /**
@@ -9,20 +17,40 @@ import { of } from 'rxjs';
   template: `
     <p>search works!</p>
     <label for="browser">Choose your browser from the list:</label>
-    <input list="browsers" name="browser" id="browser" />
+    <input list="browsers" name="browser" id="browser" #search />
     <datalist id="browsers">
       <option
         *ngFor="let browserName of browserNames$ | async"
         [value]="browserName"
       ></option>
     </datalist>
+    <input type="text" name="test" id="test" #merp />
+    <!-- <div *ngIf="input?.nativeElement">{{ input!.nativeElement.value }}</div> -->
   `,
   styles: [],
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnChanges, AfterViewInit {
   browserNames$ = of(['Edge', 'Firefox', 'Chrome', 'Opera', 'Safari']);
+  @ViewChild('search') selectBrowser?: ElementRef;
+  @ViewChild('merp') input?: ElementRef;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log({
+      ngOnInit: this.selectBrowser,
+      merp: this.input,
+    });
+  }
+
+  ngAfterViewInit(): void {
+    console.log({
+      ngOnInit: this.selectBrowser,
+      merp: this.input,
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log({ searchRef: this.selectBrowser });
+  }
 }
